f8f2b5764f6e52b1a21d2671819df577bae4cd38
Helio Elias <helioelias@gmail.com>
2018-05-20 11:01:27 -0300

    Estudo do ECMA e suas novas features

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+const port = 3000;
+
+app.listen(port, () => {
+	
+	console.log(`Started on port ${port}`); // template literals ` `
+	
+});
+
+export default app;
